ebfd302b4d1ccc1aceef96957cd2a08d476139d0
yan.xia <yan.xia@servicenow.com>
2016-09-30 16:56:20 -0700

    now can download and open the file, all method working expect the real working logic is not right, need to crate some data on backend - firebase now

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,7 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
     function ($scope, $stateParams, $http) {
-      $http.get("https://api.myjson.com/bins/1tg9s").then(function (response) {
+      $http.get("https://api.myjson.com/bins/3k1cw").then(function (response) {
         $scope.video_list = response.data;
       });
     }])
@@ -13,7 +13,7 @@ angular.module('app.controllers', [])
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
     function ($scope, $stateParams, $http) {
-      $http.get("https://api.myjson.com/bins/1wvng").then(function (response) {
+      $http.get("https://api.myjson.com/bins/1plds").then(function (response) {
         $scope.favorite_video_list = response.data;
       });
     }])
@@ -35,55 +35,68 @@ angular.module('app.controllers', [])
     }])
 
   // for file download and upload
-  .controller('FileTransferController', function ($scope, $cordovaFileTransfer) {
+  .controller('FileTransferController', function ($scope, $cordovaFileTransfer, $cordovaFileOpener2) {
 
-    $scope.testFileDownload = function () {
+    $scope.testFileDownload = function (file_url) {
 
       // File for download
       var url = "http://www.gajotres.net/wp-content/uploads/2015/04/logo_radni.png";
+      // var url = file_url;
 
       // File name only
       var filename = url.split("/").pop();
+      // var filename = temp.png;
 
       // Save location
-      var targetPath = cordova.file.externalRootDirectory + "kTbue/" + filename;
+      var targetPath = cordova.file.externalRootDirectory + "kTube/" + filename;
 
       $cordovaFileTransfer.download(url, targetPath, {}, true).then(function (result) {
-        console.log('999999999999999999999999999999999999999999999 success' + targetPath);
-
-        $scope.fileLocation = targetPath
+        $scope.play_info = "sccuessfully downloed the pic!";
+        $scope.fileLocation = targetPath;
       }, function (error) {
-        console.log('999999999999999999999999999999999999999999999 error');
-        console.log('Error');
+        $scope.play_info = "Error when download the png file" + error.message
       }, function (progress) {
         // PROGRESS HANDLING GOES HERE
       });
     };
 
-    $scope.testFileUpload = function () {
-      // Destination URL
-      var url = "http://example.gajotres.net/upload/upload.php";
-
-      //File for Upload
-      var targetPath = cordova.file.externalRootDirectory + "logo_radni.png";
-
-      // File name only
-      var filename = targetPath.split("/").pop();
-
-      var options = {
-        fileKey: "file",
-        fileName: filename,
-        chunkedMode: false,
-        mimeType: "image/jpg",
-        params: {'directory': 'upload', 'fileName': filename}
-      };
-
-      $cordovaFileTransfer.upload(url, targetPath, options).then(function (result) {
-        console.log("SUCCESS: " + JSON.stringify(result.response));
-      }, function (err) {
-        console.log("ERROR: " + JSON.stringify(err));
-      }, function (progress) {
-        // PROGRESS HANDLING GOES HERE
+    $scope.openFile = function (fileLocation) {
+      $cordovaFileOpener2.open( fileLocation,'image/png'
+      ).then(function() {
+        // Success!
+        $scope.play_info = "going to play the image"
+      }, function(err) {
+        // An error occurred. Show a message to the user
+        $scope.play_info = "some error " + err.status + " with details: " + err.message
       });
-    }
+
+    };
+    // console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
+
+    // $scope.testFileUpload = function () {
+    //   // Destination URL
+    //   var url = "http://example.gajotres.net/upload/upload.php";
+    //
+    //   //File for Upload
+    //   var targetPath = cordova.file.externalRootDirectory + "logo_radni.png";
+    //
+    //   // File name only
+    //   var filename = targetPath.split("/").pop();
+    //
+    //   var options = {
+    //     fileKey: "file",
+    //     fileName: filename,
+    //     chunkedMode: false,
+    //     mimeType: "image/jpg",
+    //     params: {'directory': 'upload', 'fileName': filename}
+    //   };
+    //
+    //   $cordovaFileTransfer.upload(url, targetPath, options).then(function (result) {
+    //     console.log("SUCCESS: " + JSON.stringify(result.response));
+    //   }, function (err) {
+    //     console.log("ERROR: " + JSON.stringify(err));
+    //   }, function (progress) {
+    //     // PROGRESS HANDLING GOES HERE
+    //   });
+    // }
   });
